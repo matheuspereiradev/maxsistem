2bd8b9261e619dac3d2c0c220e33128a902d5dd4
matheus <matheuslima20111997@gmail.com>
2021-02-05 20:32:20 -0300

    refatorando

---
+++ API/src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const port = 8080;
+const bodyParser = require('body-parser');
+const path =require('path')
+const app = express();
+
+const routerIndex = require('./routes/index')
+
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(cors());
+
+app.use('/',routerIndex);
+
+app.use('/uploads',express.static(path.join(__dirname ,'uploads')));
+
+app.listen(port,()=>{
+    console.log(`Rodando na porta ${port}`);
+});
