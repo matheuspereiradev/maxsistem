100276e59126053d3966220f92747f1707b5ce57
matheus <matheuslima20111997@gmail.com>
2021-02-21 15:52:38 -0300

    alimentandocombobox setores

--- frontend/src/pages/usuario/registerUsuario.js
+++ frontend/src/pages/usuario/registerUsuario.js
@@ -17,8 +17,7 @@ export default function RegisterUsuario(){
     const [login,setLogin] = useState('');
     const [setor,setSetor] = useState(''); 
     const [email,setEmail] = useState('');  
-    const [telefone,setTelefone] = useState(''); 
-    const [senha,setSenha] = useState(''); 
+    const [telefone,setTelefone] = useState('');
     const [isClient,setIsClient] = useState(false); 
     const [isManager,setIsManager] = useState(false); 
 
@@ -41,6 +40,10 @@ export default function RegisterUsuario(){
       api.get('company/all').then(company=>{
         setEmpresas(company.data);
       }) 
+
+      api.get('sector/all').then(sector=>{
+        setSetores(sector.data);
+      }) 
     },[])
 
     async function handleSubmit(event) {
@@ -62,17 +65,15 @@ export default function RegisterUsuario(){
                       <form onSubmit={handleSubmit}>
                         <h1>Cadastro de usuários</h1>
                         <hr/>
-                        <label>Nome:</label>
-                        <input maxLength="45" required onChange={event => { setNome(event.target.value) }} value={nome} className="input-text" type="text"/>
-                          
+                        
                         <div className="row">
                             <div className="col-50">
-                                <label>Login</label><br/>
-                                <input onChange={event => { setLogin(event.target.value) }} className="input-text" type="text" value={login}></input>
+                              <label>Nome:</label>
+                              <input maxLength="45" required onChange={event => { setNome(event.target.value) }} value={nome} className="input-text" type="text"/>
                             </div>
                             <div className="col-50 margin-left-5">
-                                <label>Email</label><br/>
-                                <input onChange={event => { setEmail(event.target.value) }} value={email} className="input-text" type="text"></input>
+                                <label>Login:</label><br/>
+                                <input onChange={event => { setLogin(event.target.value) }} value={login} className="input-text" type="text"></input>
                             </div>
                         </div>  
                         <div className="row">
@@ -80,6 +81,13 @@ export default function RegisterUsuario(){
                                 <label>Telefone</label><br/>
                                 <input onChange={event => { setTelefone(event.target.value) }} className="input-text" type="text" value={telefone}></input>
                             </div>
+                            <div className="col-50 margin-left-5">
+                                <label>Email</label><br/>
+                                <input onChange={event => { setEmail(event.target.value) }} value={email} className="input-text" type="text"></input>
+                            </div>
+                        </div>  
+                        <div className="row">
+                            
                             <div className="col-50 margin-left-5">
                               <label>Empresa:</label>
                               <select className="select-box" id="empresa" value={empresa} onChange={event=>{ setEmpresa(event.target.value)}}>
@@ -90,6 +98,17 @@ export default function RegisterUsuario(){
                                       )
                                     }))}
                               </select>
+                            </div>
+                            <div className="col-50">
+                                  <label>Setor:</label>
+                                  <select className="select-box" id="setor" value={setor} onChange={event=>{ setSetor(event.target.value)}}>
+                                        <option value="0">Nenhum setor</option>
+                                        {setores && (setores.map(set=>{
+                                          return(
+                                            <option key={set.sectorId} value={set.sectorId}>{set.sectorName}</option>
+                                          )
+                                        }))}
+                                  </select> 
                             </div>
                         </div>
                         
